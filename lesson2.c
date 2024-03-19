8acfa5d03fb4ae644e4344794715cbfd036a6ad7
devarsh2007 <sunilpatel2131@gmail.com>
2024-03-19 19:46:07 +0530

    code

---
+++ lesson2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void main()
+{
+    // 1,2,3,4,5....,10
+    // do
+    // {
+    //     /* code */
+
+    // } while ();
+
+    int count = 0;
+
+    do
+    {
+        /* code */
+        count++;
+        printf("%d", count);
+        printf("\n");
+
+    } while (count<10);
+
+    // count++;
+    // printf("%d",count);
+    // printf("\n");
+
+    // count++;
+    // printf("%d",count);
+    // printf("\n");
+
+    // count++;
+    // printf("%d",count);
+    // printf("\n");
+}
---
+++ lesson3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+    // factorial
+    // 5 - 1*2*3*4*5
+
+    int num = 5;
+
+    int count = 1;
+    // int add = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        /* code */
+        // add = count + 1;
+        count = count * (i+1);
+    }
+
+    // add=count+1;
+    // count=count*add;
+
+    printf("%d", count);
+}
