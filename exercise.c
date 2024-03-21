4dbb7486d89498e11ec70fe477638134bed0cde1
devarsh2007 <sunilpatel2131@gmail.com>
2024-03-21 10:00:21 +0530

    code

---
+++ exercise.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void main()
+{
+    for (int j = 4; j >= 1; j--)
+    {
+        for (int i = 1; i <= j; i++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    // for (int i = 1; i <= 3; i++)
+    // {
+    //     printf("* ");
+    // }
+    // // printf("* ");
+    // // printf("* ");
+    // printf("\n");
+
+    // for (int i = 1; i <= 2; i++)
+    // {
+    //     printf("* ");
+    // }
+    // // printf("* ");
+
+    // printf("\n");
+
+    // for (int i = 1; i <= 1; i++)
+    // {
+    //     printf("* ");
+    // }
+    // printf("\n");
+}
