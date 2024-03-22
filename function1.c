a0d3efd9066a19dbe57dff6bf16797cdb1f3b8ab
devarsh2007 <sunilpatel2131@gmail.com>
2024-03-22 09:45:49 +0530

    code

---
+++ function1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void printstar()
+{
+    printf("****************************************************\n");
+}
+
+
+void main()
+{
+    printstar();
+
+    printf("devarsh patel\n");
+
+    printstar();
+
+    printf("cb27\n");
+
+    printstar();
+}
