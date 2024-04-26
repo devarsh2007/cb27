625ec8590e333cb119d5b1b34756c00723a22bd1
devarsh2007 <sunilpatel2131@gmail.com>
2024-04-26 15:07:49 +0530

    code

--- function2.c
+++ function2.c
@@ -1,51 +1,14 @@
-<<<<<<< HEAD
-// write a program to print symbol.
-#include <stdio.h>
+// write a function that take a symbol from user print that symbol
 
-void printstar(char sy , int num)
-{
-    for(int i=1 ; i<=num ; i++)
-    {
-        printf("%c\n",sy);
-    }
-}
+#include <stdio.h>
 
 void main()
 {
-    char symbol=' ';
-    int num=0;
+    printf("hi");
+    char symbol;
 
-    printf("\nenter symbol : ");
+    printf("\nenter a symbol : ");
     scanf("%c",&symbol);
 
-    printf("\nenter size of symbol : ");
-    scanf("%d",&num);
-
-    printstar(symbol , num);
+    printf(symbol);
 }
-=======
-// write a program to print symbol.
-#include <stdio.h>
-
-void printstar(char sy , int num)
-{
-    for(int i=1 ; i<=num ; i++)
-    {
-        printf("%c\n",sy);
-    }
-}
-
-void main()
-{
-    char symbol=' ';
-    int num=0;
-
-    printf("\nenter symbol : ");
-    scanf("%c",&symbol);
-
-    printf("\nenter size of symbol : ");
-    scanf("%d",&num);
-
-    printstar(symbol , num);
-}
->>>>>>> 8fb1a367b09dceaceda62db3aba42823c30cccea
